442a199a5a4ef3564fa30e4aa636832c59b566a6
Josef Klafka <jlklafka@gmail.com>
2019-10-22 11:46:04 -0400

    added initial analysis and plotting

--- Scripts/all_wikipedia.sh
+++ Scripts/all_wikipedia.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-declare -a fullarr=("Sakha" "Tatar" "Uyghur" "Uzbek" "Amharic" "Zazaki"
-	"Gilaki" "Mazandarani" "Ossetian" "Pashto" "Võro" "Komi" "Vepsian" "Erzya"
-	"Assamese" "Bhojpuri" "Divehi" "Konkani" "Gujarati" "Marathi" "Odia"
-	"Maithili" "Sanskrit" "Sindhi" "Sinhalese" "Esperanto" "Interlingua"
-	"Interlingue" "Ido" "Lithuanian" "Latvian" "Samogitian" "Kannada" "Malayalam"
-	"Tamil" "Telugu" "Breton" "Irish" "Manx" "Burmese" "Georgian" "Mingrelian"
-	"Lao" "Thai" "Lezgian" "Malagasy" "Papiamentu" "Chavacano" "Albanian"
-	"Luganda" "Swahili" "Tswana" "Tsonga" "Yoruba" "Quechua" "Buryat" "Samoan"
-	"Tongan" "Nahuatl" "Somali" "Aymara" "Guarani" "Kabyle" "Hausa" "Tetum"
-	"German" "Swedish" "Russian" "English" "Serbian")
+declare -a fullarr=("English" "Mandarin")
 
   for i in "${fullarr[@]}"
   do
--- Scripts/wikipedia_process.sh
+++ Scripts/wikipedia_process.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # get the wikipedia dump file
-PREFIX=$(cat language_dict.json | jq --arg lang "$1" '.[$lang]' | tr -d \")
-curl -o datafile "https://dumps.wikimedia.org/other/cirrussearch/20190923/${PREFIX}wiki-20190923-cirrussearch-content.json.gz"
+# PREFIX=$(cat language_dict.json | jq --arg lang "$1" '.[$lang]' | tr -d \")
+# curl -o datafile "https://dumps.wikimedia.org/other/cirrussearch/20190923/${PREFIX}wiki-20190923-cirrussearch-content.json.gz"
 
 # extract the text into a single txt file with one sentence per line
 python3 Scripts/cirrus-extract.py datafile
 python3 Scripts/get_wikipedia.py $1
 rm -r text
-rm datafile
+# rm datafile
 
 # build the ngram language model in binary
 head -n 1000000 $1.txt | python3 Scripts/process_corpus.py | kenlm/build/bin/lmplz -o $2 --discount_fallback > Models/$1.arpa
@@ -19,4 +19,4 @@ python3 Scripts/wikipedia_ngrams.py $1
 Rscript Scripts/wikipedia_surprisal.R $1
 
 # clean up
-rm $1.txt Models/$1.arpa Models/$1.klm Data/wikipedia_$1.csv
+# rm $1.txt Models/$1.arpa Models/$1.klm Data/wikipedia_$1.csv
