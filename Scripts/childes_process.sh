f1940aeb8e11c7958464524285d925936485ed87
Josef Klafka <jlklafka@gmail.com>
2019-11-06 11:05:13 -0500

    word order slope analysis

--- Scripts/childes_process.sh
+++ Scripts/childes_process.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# this script takes three arguments: the name of a CHILDES collection; the column (stem or gloss);
-# and the speaker (child or adult) that you want to run the LM on
+# this script takes four arguments: the name of a CHILDES collection; the column (stem or gloss);
+# the speaker (child or adult) that you want to run the LM on; and the order of ngrams to use
 Rscript Scripts/get_childes.R $1 $2 $3
-cat Data/childes_$1_$3.txt | python3 Scripts/process_corpus.py | kenlm/build/bin/lmplz -o 3 > Models/childes_$1_$3.arpa
+# build the ngrams model
+cat Data/childes_$1_$3.txt | python3 Scripts/process_corpus.py | kenlm/build/bin/lmplz -o $4 > Models/childes_$1_$3.arpa
+# convert model to binary for faster reading and lower storage
 kenlm/build/bin/build_binary Models/childes_$1_$3.arpa Models/childes_$1_$3.klm
-rm Models/childes_$1_$3.arpa #free up space
+# get ngrams
 python3 Scripts/childes_ngrams.py $1 $3
-rm Data/childes_$1_$3.txt
+# free up space
+rm Data/childes_$1_$3.txt Models/childes_$1_$3.arpa Models/childes_$1_$3.klm
