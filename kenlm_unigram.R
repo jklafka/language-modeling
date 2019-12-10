43a31f612d93ebe4403f187dad0716fb38e27557
dyurovsky <dyurovsky@gmail.com>
2019-12-10 15:08:44 -0500

    updating unigram parser

--- kenlm_unigram.R
+++ kenlm_unigram.R
@@ -1,15 +1,42 @@
+options(warn=-1)
+options(message())
+
 library(tidyverse)
+library(tidytext)
+library(glue)
+require(here)
+
+corpus_name = commandArgs(trailingOnly=TRUE)[1]
+model_file <- here(glue("Models/{corpus_name}.lm"))
+corpus_file <- here(glue("Data/corpus/{corpus_name}.txt"))
+
 
 read_unigram_model <- function(file) {
   read_lines(file, skip = 7) %>%
     enframe(name = NULL, value = "line") %>%
     slice(1:(n()-2)) %>%
     separate(line, into = c("surprisal", "word"), sep = "\t") %>%
-    mutate(surprisal = as.numeric(surprisal))
+    mutate(surprisal = -as.numeric(surprisal))
 }
 
-get_surprisal <- function(model, unigram){
-  model %>%
-    filter(word == unigram) %>%
-    pull(surprisal)
+read_corpus <- function(file) {
+  read_lines(corpus_file) %>%
+    enframe(name = NULL, value = "text") %>%
+    mutate(length = str_count(text, pattern = "[ +]+") + 1) %>%
+    mutate(utterance_id = 1:n()) %>%
+    unnest_tokens(word, text, token = stringr::str_split, pattern = "[ +]+", 
+                  to_lower = FALSE) %>%
+    group_by(utterance_id) %>%
+    mutate(position = 1:n()) %>%
+    ungroup() %>%
+    select(-utterance_id)
 }
+
+model <- read_unigram_model(model_file)
+corpus <- read_corpus(corpus_file)
+
+surprisals <- corpus %>%
+  left_join(model, by = c("word")) %>%
+  select(-word)
+
+write_csv(surprisals, here(glue("Data/surprisals/unigram/{corpus_name}.csv")))
