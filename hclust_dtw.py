54872e09c5b0cd7f86c2ba505830499147a8a932
Josef Klafka <jlklafka@gmail.com>
2020-03-11 14:26:24 -0400

    hclust linkage

--- hclust_dtw.py
+++ hclust_dtw.py
@@ -1,16 +1,29 @@
-import csv
 import numpy as np
 import pandas as pd
 from tslearn.barycenters import dtw_barycenter_averaging
 from tslearn.metrics import dtw
+from scipy.cluster.hierarchy import linkage, dendrogram
 
 # with open("Data/5barycenters.csv", 'r') as barycenters_file:
 #     reader = csv.reader(f)
 #     for line in reader
 
+# read in barycenters and filter to unigrams
 barycenters = pd.read_csv("Data/5barycenters.csv")
-## COMPUTE PAIRWISE DTW AND STORE
-## HOW TO STORE PAIRWISE DISTANCES
-## FIND CLOSEST PAIRS
-## COMPUTE BARYCENTERS AND STORE
-## ITERATE
+barycenters = barycenters[barycenters["gram"] == "unigram"]
+# fetch languages
+languages = barycenters["language"].copy()
+# get the numerical barycenters
+centers = barycenters.loc[, 0:4].to_numpy()
+
+# linkage is the workhorse: it produces an array with pairwise clusters in the
+# first two columns, then the distance in the third column, then the number of
+# members of the created cluster in the fourth column
+Z = linkage(centers, method = "single", metric = dtw)
+
+expanded_centers = centers.copy()
+for row in Z:
+    new_center = dtw_barycenter_averaging(expanded_centers[row[0]], expanded_centers[row[1]])
+    expanded_centers = np.vstack((expanded_centers, new_center))
+## HOW TO MAP BETWEEN LOCATION ON CLUSTERING PLOT AND BARYCENTER OR LEAF NODE??
+## INTERACTIVE PLOT??
