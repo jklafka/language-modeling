3b6bfc096a75d18ef0a340b31b5d3def905e6846
Josef Klafka <jlklafka@gmail.com>
2020-03-27 14:12:02 -0400

    pdf viz of dendrogram

--- Analysis/hclust_dtw.py
+++ Analysis/hclust_dtw.py
@@ -1,12 +1,19 @@
+# @Author: Josef Klafka <academic>
+# @Date:   2020-03-27T13:34:11-04:00
+# @Email:  jlklafka@gmail.com
+# @Project: Noisy-nets
+# @Last modified by:   academic
+# @Last modified time: 2020-03-27T14:09:04-04:00
+
+
+
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from tslearn.barycenters import dtw_barycenter_averaging
 from tslearn.metrics import dtw
 from scipy.cluster.hierarchy import linkage, dendrogram
 
-# with open("Data/5barycenters.csv", 'r') as barycenters_file:
-#     reader = csv.reader(f)
-#     for line in reader
 
 # read in barycenters and filter to unigrams
 barycenters = pd.read_csv("../Data/5barycenters.csv")
@@ -20,12 +27,14 @@ centers = barycenters.loc[:, '1':'5'].to_numpy()
 # first two columns, then the distance in the third column, then the number of
 # members of the created cluster in the fourth column
 Z = linkage(centers, method = "single", metric = dtw)
-
 expanded_centers = centers.copy()
 
 for row in Z:
     cluster = np.vstack([expanded_centers[int(row[0])], expanded_centers[int(row[1])]])
     new_center = dtw_barycenter_averaging(cluster)
     expanded_centers = np.vstack((expanded_centers, new_center.T))
-## HOW TO MAP BETWEEN LOCATION ON CLUSTERING PLOT AND BARYCENTER OR LEAF NODE??
-## INTERACTIVE PLOT??
+
+## plot entire dendrogram
+plt.figure(figsize = (50, 20), dpi = 100)
+dendro = dendrogram(Z, labels = languages.to_numpy())
+plt.savefig("dendro.png")
