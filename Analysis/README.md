8402c4f7919c3157d01afb73aac35feaa0323f38
Josef Klafka <jlklafka@gmail.com>
2020-03-31 14:57:57 -0400

    init readme and argparse

---
+++ Analysis/README.md
@@ -0,0 +1,8 @@
+# Scripts for analyzing how top-down features affect the shape of the barycenters
+
+## Dendrogram
+To run hclust_viz.py, use the following command
+
+`python3 hclust_viz.py`
+
+The program may automatically open the dendrogram in a tab of your browser. If not, simply copy and paste the server address (one of the traditional localhost addresses like 127.0.0.0). 
--- Analysis/hclust_viz.py
+++ Analysis/hclust_viz.py
@@ -3,8 +3,9 @@
 # @Email:  jlklafka@gmail.com
 # @Project: Noisy-nets
 # @Last modified by:   academic
-# @Last modified time: 2020-03-31T14:46:03-04:00
+# @Last modified time: 2020-03-31T14:57:14-04:00
 
+import argparse
 import plotly.graph_objects as go # for interactive features
 import plotly.figure_factory as ff
 import numpy as np
@@ -12,9 +13,16 @@ import pandas as pd
 ## cdist_dtw does pairwise dynamic time warping on rows in a matrix
 from tslearn.metrics import cdist_dtw
 
+parser = argparse.ArgumentParser()
+parser.add_argument("gram", help="Unigram or trigram barycenters to cluster on")
+args = parser.parse_args()
+
+assert args.gram in ["unigram", "trigram"],
+    "Choose 'unigram' or 'trigram' barycenters"
+
 # read in barycenters
 barycenters = pd.read_csv("../Data/5barycenters.csv")
-barycenters = barycenters[(barycenters["gram"] == "unigram") & \
+barycenters = barycenters[(barycenters["gram"] == args.gram) & \
     (barycenters["source"] == "wikipedia")]
 
 # get the numerical barycenters
