c145d2bf3fd7e563d4da41bebec85fc8c231b50e
dyurovsky <dyurovsky@gmail.com>
2020-04-27 16:46:54 -0400

    set up bootstrapping for dba

--- Scripts/compress_surprisals.R
+++ Scripts/compress_surprisals.R
@@ -1,6 +1,8 @@
 require(tidyverse)
 require(glue)
+require(data.table)
 require(here)
+require(rsample)
 
 MIN_LENGTH <- 5
 MAX_LENGTH <- 15
@@ -11,7 +13,8 @@ MAX_LENGTH <- 15
 corpus_name <- commandArgs(trailingOnly=TRUE)[1]
 language_name <- commandArgs(trailingOnly=TRUE)[2]
 gram <- commandArgs(trailingOnly=TRUE)[3]
-surprisals <- read_csv(here(glue("Surprisals/{corpus_name}/{gram}/{language_name}.csv")))
+surprisals <- fread(here(glue("Surprisals/{corpus_name}/{gram}/{language_name}.csv"))) %>%
+  as_tibble()
 
 surprisals %>%
   filter(length >= MIN_LENGTH, length <= MAX_LENGTH, complete.cases(.)) %>%
@@ -20,9 +23,15 @@ surprisals %>%
   mutate(switch = cumsum(switch)) %>%
   group_by(length, position, switch) %>% # get ids for each sentence
   summarise(surprisal = mean(surprisal, na.rm = T)) %>%
-  summarise(surprisal = mean(surprisal, na.rm = T)) %>%
+  bootstraps(., times = 1000) %>%
+  mutate(data = map(splits, ~.x %>% analysis %>% 
+                      summarise(surprisal = mean(surprisal, na.rm = T)))) %>%
+  select(-splits) %>%
+  unnest(cols = c(data)) %>%
   ungroup() %>%
   pivot_wider(names_from = position, values_from = surprisal) %>%
   select(-length) %>%
-  write_csv(here(glue("Surprisals/{corpus_name}/{gram}/{language_name}_compressed.csv")),
-            col_names=FALSE)
+  mutate(id = as.numeric(as.factor(id))) %>%
+  mutate(id = as.numeric(as.factor(id))) %>%
+  fwrite(here(glue("Surprisals/{corpus_name}/{gram}/{language_name}_compressed.csv")),
+            col.names=FALSE)
