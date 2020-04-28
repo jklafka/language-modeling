dc9ab4827a79c5fa17873b59a60e8442ba38eb86
dyurovsky <dyurovsky@gmail.com>
2020-04-27 22:07:39 -0400

    correct resampling

--- Scripts/compress_surprisals.R
+++ Scripts/compress_surprisals.R
@@ -2,7 +2,7 @@ require(tidyverse)
 require(glue)
 require(data.table)
 require(here)
-require(rsample)
+require(tidyboot)
 
 MIN_LENGTH <- 5
 MAX_LENGTH <- 15
@@ -16,22 +16,28 @@ gram <- commandArgs(trailingOnly=TRUE)[3]
 surprisals <- fread(here(glue("Surprisals/{corpus_name}/{gram}/{language_name}.csv"))) %>%
   as_tibble()
 
-surprisals %>%
+sentence_surprisals <- surprisals %>%
   filter(length >= MIN_LENGTH, length <= MAX_LENGTH, complete.cases(.)) %>%
   mutate(switch = position < lag(position)) %>%
   mutate(switch = if_else(is.na(switch), FALSE, switch)) %>%
   mutate(switch = cumsum(switch)) %>%
   group_by(length, position, switch) %>% # get ids for each sentence
-  summarise(surprisal = mean(surprisal, na.rm = T)) %>%
-  bootstraps(., times = 1000) %>%
-  mutate(data = map(splits, ~.x %>% analysis %>% 
-                      summarise(surprisal = mean(surprisal, na.rm = T)))) %>%
-  select(-splits) %>%
-  unnest(cols = c(data)) %>%
+  summarise(surprisal = mean(surprisal, na.rm = T))
+  
+sentence_surprisals %>%
   ungroup() %>%
+  distinct(switch) %>%
+  bootstraps(1000) %>%
+  mutate(data = map(splits,~.x %>% analysis %>%
+                   right_join(sentence_surprisals, by = "switch") %>%
+                   group_by(length, position) %>%
+                   summarise(surprisal = mean(surprisal),
+                             n = n()))) %>%
+  select(-splits) %>%
+  unnest(data) %>%
+  select(id, n, length, position, surprisal) %>%
   pivot_wider(names_from = position, values_from = surprisal) %>%
   select(-length) %>%
-  mutate(id = as.numeric(as.factor(id))) %>%
   mutate(id = as.numeric(as.factor(id))) %>%
   fwrite(here(glue("Surprisals/{corpus_name}/{gram}/{language_name}_compressed.csv")),
             col.names=FALSE)
