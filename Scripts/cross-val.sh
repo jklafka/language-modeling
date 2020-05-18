9b5cc6c468c33bf3ea7d1048d95063029aaf7352
Josef Klafka <jlklafka@gmail.com>
2020-05-18 14:09:14 -0400

    fixed temp file access

--- Scripts/cross-val.sh
+++ Scripts/cross-val.sh
@@ -2,6 +2,9 @@
 
 K = 10
 
+declare -a alphabet=( 'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n'
+                      'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z')
+
 ## tokenize corpus
 cat Data/$1/$2.txt | python3 Scripts/process_corpus.py > Data/$1/${2}_processed.txt
 
@@ -12,7 +15,7 @@ split -l split_length Data/$1/${2}_processed.txt
 
 ## iterate over pieces: train and test unigram and trigram model on all but held-out piece
 ## record suprisals on held-out piece as test and write to file
-for i in `{a..f}`;
+for i in ("alphabet[@:1:${K}]");
   cat Data/$1/${2}_temp[a-f!"$i"] > Data/$1/${2}_bigtemp.txt
   do sh Scripts/process.sh Data/$1/${2}_bigtemp.txt Data/$1/${2}_temp${i}.txt
   rm Data/$1/${2}_bigtemp.txt
