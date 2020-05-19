361b700cc210f917908b5183daed9fbf13664bd0
Josef Klafka <jlklafka@gmail.com>
2020-05-19 11:31:55 -0400

    update cross-val

--- Scripts/cross-val.sh
+++ Scripts/cross-val.sh
@@ -5,15 +5,21 @@ K=10
 declare -a alphabet=( 'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n'
                       'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z' )
 
-# ## tokenize corpus
-# cat Data/$1/$2.txt | python3 Scripts/process_corpus.py > Data/$1/${2}_processed.txt
-#
+## tokenize corpus
+cat Data/$1/$2.txt | python3 Scripts/process_corpus.py > Data/$1/${2}_processed.txt
+
 ## split file into K pieces
-let split_length="`wc -l Data/$1/${2}_processed.txt | grep -oE "\d+"`/$K"
+numerator=`wc -l Data/$1/${2}_processed.txt | grep -oE "\d+"`
+let split_length=" $numerator / $K "
 
 shuf -o Data/$1/${2}_shuffled.txt Data/$1/${2}_processed.txt
 rm Data/$1/${2}_processed.txt
-split -l split_length -a 1 Data/$1/${2}_shuffled.txt
+
+if [! -d "Data/$1/$2/" ]
+then
+  mkdir Data/$1/$2/
+fi
+split -l $split_length -a 1 Data/$1/${2}_shuffled.txt Data/$1/$2/
 
 ## iterate over pieces: train and test unigram and trigram model on all but held-out piece
 ## record suprisals on held-out piece as test and write to file
