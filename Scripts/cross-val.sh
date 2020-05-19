f0aea69cd6599a1b7720139344032ea783e2a61a
Josef Klafka <jlklafka@gmail.com>
2020-05-19 10:33:07 -0400

    update bash

--- Scripts/cross-val.sh
+++ Scripts/cross-val.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-K = 10
+K=10
 
 declare -a alphabet=( 'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n'
-                      'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z')
+                      'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z' )
 
-## tokenize corpus
-cat Data/$1/$2.txt | python3 Scripts/process_corpus.py > Data/$1/${2}_processed.txt
-
+# ## tokenize corpus
+# cat Data/$1/$2.txt | python3 Scripts/process_corpus.py > Data/$1/${2}_processed.txt
+#
 ## split file into K pieces
-split_length=`wc -l "$1" | grep -oE "\d+"`/$K
+let split_length="`wc -l Data/$1/${2}.txt | grep -oE "\d+"`/$K"
 
-split -l split_length Data/$1/${2}_processed.txt
+split -l split_length -a 1 Data/$1/${2}_processed.txt
 
 ## iterate over pieces: train and test unigram and trigram model on all but held-out piece
 ## record suprisals on held-out piece as test and write to file
-for i in ("alphabet[@:1:${K}]");
-  cat Data/$1/${2}_temp[a-${K}!"${i}"] > Data/$1/${2}_bigtemp.txt
-  do sh Scripts/process.sh $1 $2 Data/$1/${2}_bigtemp.txt Data/$1/${2}_temp${i}.txt "$i"
+for letter in "${alphabet[@]:1:${K}}";
+  cat Data/$1/${2}_temp[a-${alphabet[$K]}!${letter}] > Data/$1/${2}_bigtemp.txt
+  do sh Scripts/process.sh $1 $2 Data/$1/${2}_bigtemp.txt Data/$1/${2}_temp${i}.txt $letter
   rm Data/$1/${2}_bigtemp.txt
 done
