5d2f673e4bd5b234b5323c37310981cb789b2aa2
Josef Klafka <jlklafka@gmail.com>
2020-06-17 09:39:19 -0400

    fix gather script

--- gather.sh
+++ gather.sh
@@ -6,14 +6,14 @@ for lang in "${langs[@]}"
 do
   ## do we have a unigram and trigram model folder?
   echo $lang
-  [ -d "Models/wikipedia/unigram/$lang/" ] && (echo "$lang unigram" >> to_model.txt)
-  [ -d "Models/wikipedia/trigram/$lang/" ] && (echo "$lang trigram" >> to_model.txt)
+  [ ! -d "Models/wikipedia/unigram/$lang/" ] && (echo "$lang unigram" >> to_model.txt)
+  [ ! -d "Models/wikipedia/trigram/$lang/" ] && (echo "$lang trigram" >> to_model.txt)
 
   ## do we have a non-compressed surprisal file for both unigrams and trigrams?
-  [ -f "ValSurprisals/wikipedia/unigram/$lang.csv" ] && (echo "$lang unigram" >> to_surprisal.txt)
-  [ -f "ValSurprisals/wikipedia/trigram/$lang.csv" ] && (echo "$lang trigram" >> to_surprisal.txt)
+  [ ! -f "ValSurprisals/wikipedia/unigram/$lang.csv" ] && (echo "$lang unigram" >> to_surprisal.txt)
+  [ ! -f "ValSurprisals/wikipedia/trigram/$lang.csv" ] && (echo "$lang trigram" >> to_surprisal.txt)
 
   ## do we have a compressed surprisal file for both unigrams and trigrams?
-  [ -f "ValSurprisals/wikipedia/unigram/${lang}_compressed.csv" ] && (echo "$lang unigram" >> to_compress.txt)
-  [ -f "ValSurprisals/wikipedia/trigram/${lang}_compressed.csv" ] && (echo "$lang trigram" >> to_compress.txt)
+  [ ! -f "ValSurprisals/wikipedia/unigram/${lang}_compressed.csv" ] && (echo "$lang unigram" >> to_compress.txt)
+  [ ! -f "ValSurprisals/wikipedia/trigram/${lang}_compressed.csv" ] && (echo "$lang trigram" >> to_compress.txt)
 done
