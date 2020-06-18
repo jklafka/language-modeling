1bd62ce73117e3e2ed36a518eceaf5a9c693851a
Josef Klafka <jlklafka@gmail.com>
2020-06-18 14:01:25 -0400

    more langs

--- all-crossval.sh
+++ all-crossval.sh
@@ -24,7 +24,22 @@ declare -a big_languages=("Basque" "Bosnian" "Afrikaans" "Anglo-Saxon" "Slovak"
 "Asturian" "Emilian" "Romagnol" "Extremaduran" "Franco" "Provençal"
 "Czech" "Slovenian" "Võro" "Esperanto" "Lithuanian" "Latvian"
 "Samogitian" "Chavacano" "Danish" "Indonesian" "Vietnamese" "Dutch"
-"Ukrainian" "Russian" "German" "Arabic" "Polish" "French" "Spanish")
+"Ukrainian" "Russian" "German" "Arabic" "Polish" "French" "Spanish"
+"Cantonese" "Aromanian" "Tarantino")
+
+declare -a new_languages=("Gothic" "Piedmontese"
+"Kashubian" "Cebuano" "Banyumasan" "Buginese" "Banjar" "Chamorro" "Minangkabau"
+"Gan" "Azerbaijani" "Karakalpak" "Tuvan" "Aramaic" "Tigrinya"
+"Komi-Permyak" "Udmurt" "Moksha" "Kashmiri" "Doteli" "Pali" "Romani"
+"Interlingua" "Interlingue" "Lojban" "Novial" "Volapuk" "Latgalian"
+"Cornish" "Dzongkha" "Newar" "Pontic" "Zhuang" "Avar" "Chechen" "Lak" "Kongo"
+"Kikuyu" "Lingala" "Chichewa" "Kirundi" "Kinyarwanda" "Shona" "Swati" "Sesotho"
+"Tumbuka" "Venda" "Xhosa" "Zulu" "Kalmyk" "Hawaiian" "Tahitian" "Oromo"
+"Bislama" "Norfolk" "Sranan" "Navajo" "Abkhazian" "Kabardian" "Inupiak"
+"Inuktitut" "Greenlandic" "Nauruan" "Fula" "Akan" "Ewe" "Twi" "Igbo" "Cheyenne"
+"Cree" "Cherokee" "Bambara" "Fijian" "Sango" "Afar" "Herero" "Kanuri" "Muscogee"
+"Ndonga" "Kuanyama" "Adyghe" "Choctaw" "Marshallese" "Nuosu" "Livvi-Karelian"
+"Dinka" "Atikamekw" "Ingush" "Gorontalo" "Santali")
 ## for each language
 ## submit a job to run cross-val for that language on a compute node with enough memory
 
@@ -33,7 +48,8 @@ declare -a big_languages=("Basque" "Bosnian" "Afrikaans" "Anglo-Saxon" "Slovak"
 #   sbatch one-crossval.sh wikipedia $language
 # done
 
-for language in "${big_languages[@]}"
+for language in "${new_languages[@]}"
 do
-  sbatch one-crossval.sh wikipedia $language
+  # sbatch one-crossval.sh wikipedia $language
+  sh Scripts/preprocess.sh wikipedia $language
 done
