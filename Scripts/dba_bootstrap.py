b71f0bdc08fe65d31c38269ad954e803faa30137
Josef Klafka <jlklafka@gmail.com>
2020-06-23 13:59:02 -0400

    dba_bootstrap

--- Scripts/dba_bootstrap.py
+++ Scripts/dba_bootstrap.py
@@ -3,7 +3,7 @@ import numpy as np
 from tslearn.barycenters import dtw_barycenter_averaging
 
 BARYCENTER_SIZE = 5
-OUTPUT_FILE = "Data/sbc_weighted_barycenters.csv"
+OUTPUT_FILE = "Data/crossval_barycenters.csv"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("corpus", help="Type of corpus you're working with")
@@ -13,30 +13,28 @@ args = parser.parse_args()
 
 assert args.gram in ["unigram", "trigram"], "Only accepts 'unigram' or 'trigram'"
 
-# read in surprisals data
-X = dict()
-weights = dict()
-with open("Surprisals/" + args.corpus + '/' + args.gram + '/' + \
-            args.language + "_compressed.csv", 'r') as f:
-     reader = csv.reader(f)
-     for row in reader:
-         if row[0] not in X:
-             X[row[0]] = [row[2:]]
-             weights[row[0]] = [int(row[1])]
-         else:
-             X[row[0]].append(row[2:])
-             weights[row[0]].append(int(row[1]))
+all_barycenters = []
+for i in range(1000):
+    # read in surprisals data
+    X = []
+    weights = []
+    with open("ValSurprisals/" + args.corpus + '/' + args.gram + '/' + \
+                args.language + "_compressed" + str(i) + ".csv", 'r') as f:
+         reader = csv.reader(f)
+         for row in reader:
+             X.append(row[1:])
+             weights.append(int(row[0]))
 
-# get barycenter of info-curves as list
-for key in X:
-    data = [[float(item) for item in series if item != ''] for series in X[key]]
+    # get barycenter of info-curves as list
+    data = [[float(item) for item in series if item != "NA"] for series in X]
 
     barycenter = dtw_barycenter_averaging(X = data,
                     barycenter_size = BARYCENTER_SIZE,
-                    weights = np.array(weights[key])).reshape(BARYCENTER_SIZE).tolist()
-    barycenter += [args.language, args.corpus, args.gram, key]
+                    weights = np.array(weights)).reshape(BARYCENTER_SIZE).tolist()
+    barycenter += [args.language, args.corpus, args.gram, i]
+    all_barycenters.append(barycenter)
 
-    # output barycenter to
-    with open(OUTPUT_FILE, 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow(barycenter)
+# output barycenter to
+with open(OUTPUT_FILE, 'a') as f:
+    writer = csv.writer(f)
+    writer.writerows(all_barycenters)
