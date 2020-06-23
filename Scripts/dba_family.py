b67f7543962c4ebdd9e9b4d3c4cdd330f00098a0
Josef Klafka <jlklafka@gmail.com>
2020-06-23 15:55:39 -0400

    genealogy barycenters

--- Scripts/dba_family.py
+++ Scripts/dba_family.py
@@ -10,27 +10,38 @@ grams = ["unigram", "trigram"]
 
 # read in surprisals data
 X = []
-families = []
-genii = []
+# genealogy is a dict mapping families to lists of genii
+genealogy = {}
+
 with open("Data/families_barycenters.csv", 'r') as f:
      reader = csv.reader(f)
      for row in reader:
          X.append(row)
-         families.append(row[-2])
-         genii.append(row[-1])
+         if row[-2] not in genealogy:
+             genealogy[row[-2]] = []
+         genealogy[row[-2]].append(row[-1])
 
-families = list(set(families))
-genii = list(set(genii))
+barycenters = []
 
-# get barycenter of info-curves as list
-family_barycenters = []
-genus_barycenters = []
+for gram in grams:
+    ## create gram mean barycenter
+    data = [row[:5] for row in X if row[-3] == gram]
+    data = [[float(item) for item in series] for series in data]
 
-for family in families:
-    for gram in grams:
-        print("family", family, gram)
+    data =  [[el - sum(row) / len(row) for el in row] for row in data]
+
+    if data != []:
+        barycenter = dtw_barycenter_averaging(X = data,
+                        barycenter_size = BARYCENTER_SIZE,
+                        verbose = True).reshape(BARYCENTER_SIZE).tolist()
+        barycenter += ["mean", "mean", gram]
+        barycenters.append(barycenter)
+
+    for family in genealogy:
+        ## create family mean barycenter
+        # print("family", family, gram)
         data = [row[:5] for row in X if row[-2] == family and row[-3] == gram]
-        data = [[float(item) for item in series if item != "NA"] for series in data]
+        data = [[float(item) for item in series] for series in data]
 
         data =  [[el - sum(row) / len(row) for el in row] for row in data]
 
@@ -38,31 +49,26 @@ for family in families:
             barycenter = dtw_barycenter_averaging(X = data,
                             barycenter_size = BARYCENTER_SIZE,
                             verbose = True).reshape(BARYCENTER_SIZE).tolist()
-            barycenter += [family, gram]
-            family_barycenters.append(barycenter)
+            barycenter += [family, "mean", gram]
+            barycenters.append(barycenter)
 
-for genus in genii:
-    for gram in grams:
-        print("genus", genus, gram)
-        data = [row[:5] for row in X if row[-1] == genus and row[-3] == gram]
-        data = [[float(item) for item in series if item != "NA"] for series in data]
+        for genus in genealogy[family]:
+            ## create genus mean barycenter
+            data = [row[:5] for row in X if row[-1] == genus and row[-3] == gram]
+            data = [[float(item) for item in series] for series in data]
 
-        data =  [[el - sum(row) / len(row) for el in row] for row in data]
+            data =  [[el - sum(row) / len(row) for el in row] for row in data]
 
-        if data != []:
-            barycenter = dtw_barycenter_averaging(X = data,
-                            barycenter_size = BARYCENTER_SIZE,
-                            verbose = True).reshape(BARYCENTER_SIZE).tolist()
-            barycenter += [genus, gram]
-            genus_barycenters.append(barycenter)
+            if data != []:
+                barycenter = dtw_barycenter_averaging(X = data,
+                                barycenter_size = BARYCENTER_SIZE,
+                                verbose = True).reshape(BARYCENTER_SIZE).tolist()
+                barycenter += [family, genus, gram]
+                barycenters.append(barycenter)
+
 
 # output barycenter to
-with open("Data/by_family_barycenters.csv", 'w') as f:
+with open("Data/genealogy_barycenters.csv", 'w') as f:
     writer = csv.writer(f)
-    writer.writerow([1,2,3,4,5,"family","gram"])
-    writer.writerows(family_barycenters)
-
-with open("Data/by_genus_barycenters.csv", 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow([1,2,3,4,5,"genus","gram"])
-    writer.writerows(genus_barycenters)
+    writer.writerow([1,2,3,4,5,"family", "genus", "gram"])
+    writer.writerows(barycenters)
